555755583ccf332d454811addbb7ef6abe7fdc3d
Syed Shoieb Javeed <shoiebjaveed@gmail.com>
2023-05-06 00:12:10 +0530

    first server

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-//Arrays
 
-const hobbies = ['apple', 'oranges', '', 'mango', '', 'lemon'];
+const http = require('http'); //importing core module of node
 
-// for(let hobby of hobbies) {
-//     console.log(hobby);
-// }
-const newHobbies = hobbies.map((hobby) => {return hobby === '' ? 'empty string' : hobby});
+// const reqListener = (req, res) => {
+// } this an example to create a request listner
 
-console.log(newHobbies)
-
-const obj1 = {'key1': 1 , 'key2' : 2}
-
-const obj2 = { ...obj1, key1: 1000}
-
-console.log(obj1)
-
-console.log(obj2)
-
-//Rest Operator
-
-const toArray = (...args) => {
-    return args
-}
-
-console.log(toArray(1,3,4,5,6,6))
-
-//Destructuring
-const person = {
-    name: "Max",
-    age: 24,
-    greet() {
-        console.log("Hi, I'm " + this.name)
+const server = http.createServer((req, res) => { //
+    // console.log(req.url, req.method, req.headers);
+    // console.log("shoieb");
+    // process.exit(); stops the server
+    const url = req.url;
+    res.setHeader('Content-Type', 'text/html'); //helps to set the header
+    if(url === '/'){
+        res.write('<html>'); //helps in writing html in document
+        res.write('<head><title>my first document</title></head>');
+        res.write('<body><h1>this is my first response</h1></body>');
+        res.write('</html>');
+        return res.end(); //helps in exit the response   
     }
-}
+    if(url === '/home'){
+        res.write('<html>'); //helps in writing html in document
+        res.write('<head><title>my first document</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end(); //helps in exit the response   
+    }
+    if(url === '/about'){
+        res.write('<html>'); //helps in writing html in document
+        res.write('<head><title>my first document</title></head>');
+        res.write('<body><h1>Welcome to about us</h1></body>');
+        res.write('</html>');
+        return res.end(); //helps in exit the response   
+    }
+    if(url === '/node'){
+        res.write('<html>'); //helps in writing html in document
+        res.write('<head><title>my first document</title></head>');
+        res.write('<body><h1>Welcome to my node js project</h1></body>');
+        res.write('</html>');
+        return res.end(); //helps in exit the response   
+    }
+});
 
-const sayHi = ({ greet }) => {
-    console.log(greet())
-}
+server.listen(4000); //listens to the port we enter
 
-sayHi(person)
 
-const {name , age} = person;
-console.log(name, age)
