1b230b7e270b5dcd5de57a89d92cc2e40239b0cd
Syed Shoieb Javeed <shoiebjaveed@gmail.com>
2023-05-05 14:51:47 +0530

    async/await examples

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
-//Arrays
-
-const hobbies = ['apple', 'oranges', '', 'mango', '', 'lemon'];
-
-// for(let hobby of hobbies) {
-//     console.log(hobby);
-// }
-const newHobbies = hobbies.map((hobby) => {return hobby === '' ? 'empty string' : hobby});
-
-console.log(newHobbies)
-
-const obj1 = {'key1': 1 , 'key2' : 2}
-
-const obj2 = { ...obj1, key1: 1000}
-
-console.log(obj1)
-
-console.log(obj2)
-
-//Rest Operator
-
-const toArray = (...args) => {
-    return args
+async function start() {
+  console.log("a");
+  console.log("b");
+  const b = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(console.log("c")), 3000);
+  })
+  await b;
+  console.log("d");
 }
 
-console.log(toArray(1,3,4,5,6,6))
-
-//Destructuring
-const person = {
-    name: "Max",
-    age: 24,
-    greet() {
-        console.log("Hi, I'm " + this.name)
-    }
+async function startTwo() {
+console.log('a');
+console.log('b');
+const c = new Promise((res, rej) => {
+    setTimeout(() => res(console.log('c')), 3000)
+})
+await c
+const d = new Promise((res, rej) => {
+    setTimeout(() => res(console.log('d')), 0)
+})
+await d
+console.log('e');
 }
-
-const sayHi = ({ greet }) => {
-    console.log(greet())
-}
-
-sayHi(person)
-
-const {name , age} = person;
-console.log(name, age)
+startTwo();
