f874c0056b58db1005ac50e34af392d51759c27d
Syed Shoieb Javeed <shoiebjaveed@gmail.com>
2023-05-08 22:42:44 +0530

    testing write filesync

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const http = require('http'); //importing core module of node
+const fs = require('fs');
 
 // const reqListener = (req, res) => {
 // } this an example to create a request listner
@@ -9,11 +10,12 @@ const server = http.createServer((req, res) => { //
     // console.log("shoieb");
     // process.exit(); stops the server
     const url = req.url;
+    const method = req.method;
     res.setHeader('Content-Type', 'text/html'); //helps to set the header
     if(url === '/'){
         res.write('<html>'); //helps in writing html in document
         res.write('<head><title>my first document</title></head>');
-        res.write('<body><h1>this is my first response</h1></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"</input><button type="submit">submit</button></form></body>');
         res.write('</html>');
         return res.end(); //helps in exit the response   
     }
@@ -37,6 +39,23 @@ const server = http.createServer((req, res) => { //
         res.write('<body><h1>Welcome to my node js project</h1></body>');
         res.write('</html>');
         return res.end(); //helps in exit the response   
+    }
+    if(url  === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync('message.txt', message);
+            console.log(parsedBody);
+        });
+         res.statusCode = 302;
+         res.setHeader('Location', '/');
+         return res.end();  
+
     }
 });
 
